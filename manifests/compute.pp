230f7deb4f93b30ac727f7a70768b9f44abb3a35
Andy Edmonds <andy@edmonds.be>
2013-10-07 16:58:18 +0200

    fixes var scopes

--- manifests/compute.pp
+++ manifests/compute.pp
@@ -3,32 +3,33 @@ class icclab::compute{
   include icclab::params
 
   class {'openstack::compute':
+
     #network
     internal_address        => $ipaddress_eth0,
-    private_interface       => $private_interface, #eth0
+    private_interface       => $icclab::params::private_interface, #eth0
 
     #quantum
     ovs_local_ip            => $ipaddress_eth0,
-    quantum_auth_url        => "http://${controller_node_int_address}:35357/v2.0",
-    keystone_host           => $controller_node_int_address,
-    quantum_host            => $controller_node_int_address,
+    quantum_auth_url        => "http://${icclab::params::controller_node_int_address}:35357/v2.0",
+    keystone_host           => $icclab::params::controller_node_int_address,
+    quantum_host            => $icclab::params::controller_node_int_address,
 
     #passwords
-    rabbit_password         => $one_to_rule_them_all,
-    nova_user_password      => $one_to_rule_them_all,
-    nova_db_password        => $one_to_rule_them_all,
-    quantum_user_password   => $one_to_rule_them_all,
-    cinder_db_password      => $one_to_rule_them_all,
+    rabbit_password         => $icclab::params::one_to_rule_them_all,
+    nova_user_password      => $icclab::params::one_to_rule_them_all,
+    nova_db_password        => $icclab::params::one_to_rule_them_all,
+    quantum_user_password   => $icclab::params::one_to_rule_them_all,
+    cinder_db_password      => $icclab::params::one_to_rule_them_all,
 
     #database
-    db_host                 => $controller_node_int_address,
+    db_host                 => $icclab::params::controller_node_int_address,
 
     #misc
-    libvirt_type            => 'qemu',
+    libvirt_type            => $icclab::params::libvirt,
     setup_test_volume       => true,
-    verbose                 => true,
-    rabbit_host             => $controller_node_int_address,
-    glance_api_servers      => "${controller_node_int_address}:9292",
-    vncproxy_host           => $controller_node_int_address,
+    verbose                 => $icclab::params::verbose,
+    rabbit_host             => $icclab::params::controller_node_int_address,
+    glance_api_servers      => "${icclab::params::controller_node_int_address}:9292",
+    vncproxy_host           => $icclab::params::controller_node_int_address,
   }
 }
--- manifests/params.pp
+++ manifests/params.pp
@@ -4,5 +4,8 @@ class icclab::params{
 	$one_to_rule_them_all = 'admin'
 	$controller_node_int_address  = '10.10.100.51'
 	$private_interface = 'eth0'
+	$verbose = true
+	# set to 'kvm' if deploying on metal
+	$libvirt = 'qemu'
   /* --------------------------------------------------*/
 }
