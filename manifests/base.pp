2c9fd670ba61af1771522c7e56f18b832a21b2b3
Andy Edmonds <andy@edmonds.be>
2013-10-19 01:33:03 +0200

    fixes newrelic class

--- manifests/base.pp
+++ manifests/base.pp
@@ -7,9 +7,10 @@ class icclab::base {
 
 	# TODO: fixme, I timeout on the 1st run, not the 2nd
 	if $icclab::params::NewRelic_API_Key {
-		newrelic::server { 'controller': 
-			newrelic_license_key => $icclab::params::NewRelic_API_Key,
-		}
+		class { 'newrelic': license => $icclab::params::NewRelic_API_Key }
+		#newrelic::server { 'controller': 
+		#	newrelic_license_key => $icclab::params::NewRelic_API_Key,
+		#}
 	}else {
 		warning ('Cannot install newrelic, NewRelic_API_Key is not set')
 	}
