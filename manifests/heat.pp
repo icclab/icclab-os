41fdd4ba37609bc8623b6f00a776e53b0636675a
Andy Edmonds <andy@edmonds.be>
2013-11-12 21:01:12 +0100

    parameterised heat manifest

--- manifests/heat.pp
+++ manifests/heat.pp
@@ -1,6 +1,6 @@
-# Class: icclab::heat
-#
-#
+
+# TODO test me!
+
 class icclab::heat {
 	
 	include icclab::params
@@ -8,24 +8,58 @@ class icclab::heat {
 	class { 'heat::db::mysql':
 		password => $icclab::params::one_to_rule_them_all,
 		host     => $icclab::params::controller_node_int_address,
-	}
+	} ->
 
 	class {'heat::keystone::auth':
 		password          => $icclab::params::one_to_rule_them_all,
-	}
+		public_address    => $icclab::params::controller_node_ext_address,
+		admin_address     => $icclab::params::controller_node_int_address,
+		internal_address  => $icclab::params::controller_node_int_address,
+	} ->
+
+	class {'heat::keystone::auth_cfn':
+		password           => $icclab::params::one_to_rule_them_all,
+		public_address     => $icclab::params::controller_node_ext_address,
+		admin_address      => $icclab::params::controller_node_int_address,
+		internal_address   => $icclab::params::controller_node_int_address,
+	} ->
 
 	class { 'heat':
 		keystone_password => $icclab::params::one_to_rule_them_all,
 		sql_connection    => 'mysql://heat:${icclab::params::one_to_rule_them_all}@${icclab::params::controller_node_int_address}/heat',
 		auth_uri          => 'http://${icclab::params::controller_node_int_address}:5000/v2.0',
-		keystone_host     => $icclab::params::controller_node_int_address
-		verbose           => $icclab::params::verbose
-		rabbit_host       => $icclab::params::controller_node_int_address
+		keystone_host     => $icclab::params::controller_node_int_address,
+		verbose           => $icclab::params::verbose,
+		rabbit_host       => $icclab::params::controller_node_int_address,
 		rabbit_userid     => $icclab::params::one_to_rule_them_all,
 		rabbit_password   => $icclab::params::one_to_rule_them_all,
+	} ->
+
+	class { 'heat::engine': 
+		heat_stack_user_role          => 'heat_stack_user',
+	  	heat_metadata_server_url      => "http://${icclab::params::controller_node_int_address}:8000",
+	  	heat_waitcondition_server_url => "http://${icclab::params::controller_node_int_address}:8000/v1/waitcondition",
+	  	heat_watch_server_url         => "http://${icclab::params::controller_node_int_address}:8003",
 	}
 
-	class { 'heat::engine': }
+	# heat's native API
+	class { 'heat::api': 
+		bind_host         => '0.0.0.0',
+  		bind_port         => '8004',
+	}
 
-	class { 'heat::api': }
+	# heat's cloudformation API
+	class { 'heat::api_cfn': 
+		bind_host         => '0.0.0.0',
+	  	bind_port         => '8000',
+	}
+
+	# heat's cloudwatch API
+	class { 'heat::api_cloudwatch': 
+		bind_host         => '0.0.0.0',
+  		bind_port         => '8003',
+	}
+
+	class { 'heat::client': }
+
 }
